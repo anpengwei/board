c5bff2912ebb1010bc962c5ea497160e5917be49
anpengwei <1553147738@qq.com>
2020-10-17 16:29:41 +0800

    新增路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    测试
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>新路径</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  // 定义属性
+  data() {
+    return {
+      a: 1,
+    }
+  },
+  computed: {
+    number: {
+      get() {
+        return this.a + 1
+      },
+      set() {},
+    },
+  },
+  watch: {},
+  methods: {
+    init() {},
+  },
+  created() {
+    this.init()
+  },
+  mounted() {},
+}
+</script>
+
+<style lang="less" scoped></style>
